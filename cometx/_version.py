1af7336f7b76975a9db1b7faaa43737f61860d7d
Doug Blank <doug.blank@gmail.com>
2024-08-25 08:18:26 -0400

    Version 2.1.11

--- cometx/_version.py
+++ cometx/_version.py
@@ -12,5 +12,5 @@
 #  the express permission of Comet ML Inc.
 # *******************************************************
 
-version_info = (2, 1, 10)
+version_info = (2, 1, 11)
 __version__ = ".".join(map(str, version_info))
