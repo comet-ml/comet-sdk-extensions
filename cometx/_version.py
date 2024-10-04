c538fc3b73766835100909998d47c1a86467fe4e
Douglas Blank <doug@comet.com>
2024-10-04 13:20:15 -0400

    Version 2.2.15, working admin chargeback-report

--- cometx/_version.py
+++ cometx/_version.py
@@ -12,5 +12,5 @@
 #  the express permission of Comet ML Inc.
 # *******************************************************
 
-version_info = (2, 2, 14)
+version_info = (2, 2, 15)
 __version__ = ".".join(map(str, version_info))
